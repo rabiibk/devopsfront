2744d789a69d954b25fc4d44c122a46495ce4da5
rabiibk <rabii.benkhlifa@esprit.tn>
2024-02-07 13:45:09 +0000

    cors front complet

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://192.168.12.150:8082';
+  private apiUrl = 'http://192.168.12.150:8090';
 
   constructor(private http: HttpClient) { }
 
